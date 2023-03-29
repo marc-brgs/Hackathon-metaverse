b051c07466d27533861c7f0ad323f53673918a22
MatthieuL <61740378+Matthieu39@users.noreply.github.com>
2023-03-29 16:09:26 +0200

    Press to extinguish

--- EFF Simulator/Assets/Scripts/Extinguisher.cs
+++ EFF Simulator/Assets/Scripts/Extinguisher.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,50 @@ using UnityEngine;
 public class Extinguisher : MonoBehaviour
 {
     [SerializeField] private float extinguishForce = 0.1f; // per second
+
+    [SerializeField] private ParticleSystem Hose;
     
+    private bool isEsting;
+
+    public void Start()
+    {
+        
+    }
+
     void Update()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 10f) && hit.collider.TryGetComponent(out Fire fire))
+        var emissionModule = Hose.emission;
+        
+        if (Input.GetMouseButtonDown(0))
         {
-            fire.TryExtinguish(extinguishForce * Time.deltaTime);
+            isEsting = true;
         }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isEsting = false;
+        }
+
+        if (isEsting)
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 10f) && hit.collider.TryGetComponent(out Fire fire))
+            {
+                fire.TryExtinguish(extinguishForce * Time.deltaTime);
+            }
+            emissionModule.rateOverTime = 30;
+        }
+
+
+        if (!isEsting)
+        {
+            emissionModule.rateOverTime = 0;
+        }
+        
+        
+        
+           
+
+
+
+
     }
 }
