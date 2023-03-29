c91bdef88eea77024bfb05242482cffce148ee31
Marc Brgs <marc.brgs@yahoo.fr>
2023-03-29 16:38:59 +0200

    Active and disable extinguisher

--- EFF Simulator/Assets/Scripts/Extinguisher.cs
+++ EFF Simulator/Assets/Scripts/Extinguisher.cs
@@ -2,54 +2,42 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class Extinguisher : MonoBehaviour
 {
     [SerializeField] private float extinguishForce = 0.1f; // per second
 
-    [SerializeField] private ParticleSystem Hose;
+    [SerializeField] private ParticleSystem hose;
     
-    private bool isEsting;
+    private bool isActive;
 
-    public void Start()
+    private void Start()
     {
-        
+        isActive = false;
+        var emission = hose.emission;
+        emission.rateOverTime = 0;
     }
 
     void Update()
     {
-        var emissionModule = Hose.emission;
-        
-        if (Input.GetMouseButtonDown(0))
+        if (isActive && Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 10f) && hit.collider.TryGetComponent(out Fire fire))
         {
-            isEsting = true;
+            fire.TryExtinguish(extinguishForce * Time.deltaTime);
         }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            isEsting = false;
-        }
+    }
 
-        if (isEsting)
-        {
-            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 10f) && hit.collider.TryGetComponent(out Fire fire))
-            {
-                fire.TryExtinguish(extinguishForce * Time.deltaTime);
-            }
-            emissionModule.rateOverTime = 30;
-        }
+    public void Activate()
+    {
+        var emission = hose.emission;
+        isActive = true;
+        emission.rateOverTime = 30;
+    }
 
-
-        if (!isEsting)
-        {
-            emissionModule.rateOverTime = 0;
-        }
-        
-        
-        
-           
-
-
-
-
+    public void Disable()
+    {
+        var emission = hose.emission;
+        isActive = false;
+        emission.rateOverTime = 0;
     }
 }
