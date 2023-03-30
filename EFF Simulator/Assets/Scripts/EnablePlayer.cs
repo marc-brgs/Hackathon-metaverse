e9afabf1767d3d7f1d3143809b84baceb711ddb0
Marc Brgs <marc.brgs@yahoo.fr>
2023-03-30 17:23:52 +0200

    Enable camera after joining room

--- EFF Simulator/Assets/Scripts/EnablePlayer.cs
+++ EFF Simulator/Assets/Scripts/EnablePlayer.cs
@@ -13,9 +13,14 @@ public class EnablePlayer : MonoBehaviour
     [SerializeField] private MonoBehaviour playerMovement;
     [SerializeField] private MonoBehaviour leftXRController;
     [SerializeField] private MonoBehaviour leftXRRayInteractor;
+    [SerializeField] private LineRenderer leftLineRenderer;
+    [SerializeField] private MonoBehaviour leftInteractorLineVisual;
     [SerializeField] private MonoBehaviour rightXRController;
     [SerializeField] private MonoBehaviour rightXRRayInteractor;
+    [SerializeField] private LineRenderer rightLineRenderer;
+    [SerializeField] private MonoBehaviour rightInteractorLineVisual;
     [SerializeField] private GameObject XRDeviceSimulator;
+    [SerializeField] private GameObject mainCamera;
     
     // Start is called before the first frame update
     void Start()
@@ -28,9 +33,14 @@ public class EnablePlayer : MonoBehaviour
             playerMovement.enabled = true;
             leftXRController.enabled = true;
             leftXRRayInteractor.enabled = true;
+            leftLineRenderer.enabled = true;
+            leftInteractorLineVisual.enabled = true;
             rightXRController.enabled = true;
             rightXRRayInteractor.enabled = true;
+            rightLineRenderer.enabled = true;
+            rightInteractorLineVisual.enabled = true;
             XRDeviceSimulator.SetActive(true);
+            mainCamera.SetActive(true);
         }
     }
 }
